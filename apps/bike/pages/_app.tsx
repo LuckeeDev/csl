54eb454b56ef186beec86ea73b9e8c5f4acbb761
Luca Zoppetti <zoppe2304@gmail.com>
2021-02-01 19:35:52 +0100

    chore(bike): removed useless files

--- apps/bike/pages/_app.tsx
+++ apps/bike/pages/_app.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to bike!</title>
+        <title>CSL | Green Lane Project</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to bike!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <main>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
