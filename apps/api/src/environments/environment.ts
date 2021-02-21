a36599f029bc1f50aac032572a0e17257a90c882
Luca Zoppetti <zoppe2304@gmail.com>
2021-02-21 21:17:44 +0100

    fix: og:image

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -20,5 +20,3 @@ export const environment: { [x: string]: any } = {
   FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
 };
-
-console.log(environment.FIREBASE_PRIVATE_KEY);
