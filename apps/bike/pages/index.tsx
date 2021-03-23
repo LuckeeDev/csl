57f6c22b8a31ac998ba16490b20079518c7901f0
Luca Zoppetti <zoppe2304@gmail.com>
2021-03-23 09:28:56 +0100

    chore: :arrow_up: migrated to latest and removed unused apps

--- apps/bike/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Index() {
-  return <h1>Welcome to the Green Lane Project home!</h1>;
-}
-
-export default Index;
--- apps/client/src/app/global/http/api.interceptor.ts
+++ apps/client/src/app/global/http/api.interceptor.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+	HttpEvent,
+	HttpHandler,
+	HttpInterceptor,
+	HttpRequest,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    const updatedRequest = req.clone({
-      url: `${environment.api}${req.url}`,
-      withCredentials: true,
-    });
+	intercept(
+		req: HttpRequest<any>,
+		next: HttpHandler
+	): Observable<HttpEvent<any>> {
+		const updatedRequest = req.clone({
+			url: `${environment.api}${req.url}`,
+			withCredentials: true,
+		});
 
-    return next.handle(updatedRequest);
-  }
+		return next.handle(updatedRequest);
+	}
 }
