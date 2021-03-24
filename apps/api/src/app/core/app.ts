7b6f4020040fbbed0eb6b0d6a5447054785d8d2f
Luca Zoppetti <zoppe2304@gmail.com>
2021-03-24 15:52:31 +0100

    chore: added googleapis

--- apps/api/src/app/core/app.ts
+++ apps/api/src/app/core/app.ts
@@ -7,40 +7,69 @@ import * as fileUpload from 'express-fileupload';
 import { webhookHandler } from '@common/utils';
 import PackageJSON from '../../../../../package.json';
 
+import { google, calendar_v3 } from 'googleapis';
+import { v4 } from 'uuid';
+
 export function setupApp(app: Application) {
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:4200',
-        'https://cslussana.com',
-        'https://beta.cslussana.com',
-      ],
-      credentials: true,
-    })
-  );
+	app.use(
+		cors({
+			origin: [
+				'http://localhost:4200',
+				'https://cslussana.com',
+				'https://beta.cslussana.com',
+			],
+			credentials: true,
+		})
+	);
 
-  app.use(
-    cookieSession({
-      maxAge: 24 * 60 * 60 * 1000,
-      keys: env.COOKIE_KEYS,
-    })
-  );
+	app.use(
+		cookieSession({
+			maxAge: 24 * 60 * 60 * 1000,
+			keys: env.COOKIE_KEYS,
+		})
+	);
 
-  app.use(passport.initialize());
-  app.use(passport.session());
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-  // Receive webhooks from Stripe [TODO: need to add CORS, allowing Stripe, to this route]
-  app.post('/api/webhook', raw({ type: 'application/json' }), webhookHandler);
+	const calendar = google.calendar({
+		version: 'v3',
+		auth: 'key',
+	});
 
-  app.use(json());
+	calendar.events.insert({
+		conferenceDataVersion: 1,
+		requestBody: {
+			summary: 'Test event',
+			start: {
+				timeZone: 'Europe/Rome'
+			},
+			end: {
 
-  app.use(
-    fileUpload({
-      createParentPath: true,
-    })
-  );
+			},
+			conferenceData: {
+				createRequest: {
+					requestId: v4(),
+					conferenceSolutionKey: {
+						type: 'hangoutsMeet',
+					},
+				},
+			},
+		},
+	});
 
-  app.get('/version', (req, res) => {
-    res.send(`Current version is ${PackageJSON.version}`);
-  });
+	// Receive webhooks from Stripe [TODO: need to add CORS, allowing Stripe, to this route]
+	app.post('/api/webhook', raw({ type: 'application/json' }), webhookHandler);
+
+	app.use(json());
+
+	app.use(
+		fileUpload({
+			createParentPath: true,
+		})
+	);
+
+	app.get('/version', (req, res) => {
+		res.send(`Current version is ${PackageJSON.version}`);
+	});
 }
