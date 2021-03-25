37e06fd9f18c95d229e7392210a9952f8c3acbd4
Luca Zoppetti <zoppe2304@gmail.com>
2021-03-25 21:32:49 +0100

    fix: :ambulance: ignore all netlify builds

--- scripts/netlify-ignore.sh
+++ scripts/netlify-ignore.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-LAST_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --max-count=1`)
-echo $LAST_TAG
+# LAST_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --max-count=1`)
+# echo $LAST_TAG
 
-PREVIOUS_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
-echo $PREVIOUS_TAG
+# PREVIOUS_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
+# echo $PREVIOUS_TAG
 
-echo Getting diff between $LAST_TAG and $PREVIOUS_TAG
-HAS_DIFF=$(git diff --quiet $LAST_TAG $PREVIOUS_TAG apps/api/src/ || echo "true")
+# echo Getting diff between $LAST_TAG and $PREVIOUS_TAG
+# HAS_DIFF=$(git diff --quiet $LAST_TAG $PREVIOUS_TAG apps/api/src/ || echo "true")
 
-if [ $HAS_DIFF = "true" ]
-then
-  echo "Last two tags have diff"
-  exit 1
-else
-  echo "Last two tags have no diff"
-  exit 0
-fi
+# if [ $HAS_DIFF = "true" ]
+# then
+#   echo "Last two tags have diff"
+#   exit 1
+# else
+#   echo "Last two tags have no diff"
+#   exit 0
+# fi
+exit 0
