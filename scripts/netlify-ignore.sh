39c5b338bf5df08da5c83f8511c1b261e70da2f8
Luca Zoppetti <zoppe2304@gmail.com>
2021-03-25 09:03:25 +0100

    ci: :green_heart: new netlify ignore script

---
+++ scripts/netlify-ignore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+LAST_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --max-count=1`)
+echo $LAST_TAG
+
+PREVIOUS_TAG=$(git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`)
+echo $PREVIOUS_TAG
+
+echo Getting diff between $LAST_TAG and $PREVIOUS_TAG
+HAS_DIFF=$(git diff --quiet $LAST_TAG $PREVIOUS_TAG apps/api/src/ || echo "true")
+
+if [ $HAS_DIFF = "true" ]
+then
+  echo "Last two tags have diff"
+  exit 1
+else
+  echo "Last two tags have no diff"
+  exit 0
+fi
