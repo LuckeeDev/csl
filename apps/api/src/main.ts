750d4597d0dc9338a054bf3848548bf3e33d3b03
Luca Zoppetti <zoppe2304@gmail.com>
2021-03-30 19:14:08 +0200

    chore(release): 1.10.13

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,9 @@
-// Main imports
 import { environment as env } from '@environments/environment';
 import * as http from 'http';
 import * as io from 'socket.io';
 import { app } from '@core';
 import { setupSocket } from './app/core/socket';
 
-// Start server and socket
 const port = env.PORT;
 const server = http.createServer(app);
 const socket = io(server, { path: '/socket' });
