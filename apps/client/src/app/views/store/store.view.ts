98f2300500e722667c022b51446581c1854e8b3c
Luca Zoppetti <zoppe2304@gmail.com>
2021-04-08 20:29:43 +0200

    fix: show payments link if user isRappreDiClasse

--- apps/client/src/app/views/store/store.view.ts
+++ apps/client/src/app/views/store/store.view.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
-import { IDashboardLink } from '@csl/shared';
+import { AuthState } from '@/global/store/auth';
+import { Component, OnInit } from '@angular/core';
+import { IDashboardLink, IUser } from '@csl/shared';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
 	selector: 'csl-store-home',
 	templateUrl: './store.view.html',
 	styleUrls: ['./store.view.scss'],
 })
-export class StoreView {
-	links: IDashboardLink[] = [
+export class StoreView implements OnInit {
+	@Select(AuthState.user)
+	user$: Observable<IUser>;
+
+	defaultLinks: IDashboardLink[] = [
 		{ link: 'gadgets', title: 'Gadget' },
 		{ link: 'photos', title: 'Foto' },
 		{ link: 'summary', title: 'Riepilogo' },
 	];
+
+	rappreLinks: IDashboardLink[] = [{ link: 'payments', title: 'Pagamenti' }];
+
+	links$: Observable<IDashboardLink[]>;
+
+	ngOnInit() {
+		this.links$ = this.user$.pipe(
+			map((user) => user.isRappreDiClasse),
+			map((isRappreDiClasse) => {
+				if (isRappreDiClasse) {
+					return [...this.defaultLinks, ...this.rappreLinks];
+				} else {
+					return this.defaultLinks;
+				}
+			})
+		);
+	}
 }
