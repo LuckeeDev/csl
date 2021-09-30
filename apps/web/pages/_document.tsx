72b9a534bab04e73afcb1295fae75ff852e8b95c
Luca Zoppetti <luca@luckee.dev>
2021-09-30 16:00:29 +0200

    chore: created nextjs app

---
+++ apps/web/pages/_document.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/display-name */
+import { ReactElement } from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class CustomDocument extends Document<{
+	styleTags: ReactElement[];
+}> {
+	static getInitialProps({ renderPage }) {
+		const sheet = new ServerStyleSheet();
+
+		const page = renderPage(
+			(App) => (props) => sheet.collectStyles(<App {...props} />)
+		);
+
+		const styleTags = sheet.getStyleElement();
+
+		return { ...page, styleTags };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					{this.props.styleTags}
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					/>
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/icon?family=Material+Icons"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
