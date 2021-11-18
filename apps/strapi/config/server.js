b30ad2c44967aa495d268d949b5dec709546f98e
Luca Zoppetti <luca@luckee.dev>
2021-11-18 18:37:19 +0100

    fix: deploy admin and api separately

--- apps/strapi/config/server.js
+++ apps/strapi/config/server.js
@@ -3,8 +3,10 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('STRAPI_URL', 'http://localhost:1337'),
   admin: {
+    url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+    serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ad847df82b5721bb3fbe8f169958ccde'),
-    },
+		secret: env('ADMIN_JWT_SECRET', 'ad847df82b5721bb3fbe8f169958ccde'),
+	},
   },
 });
