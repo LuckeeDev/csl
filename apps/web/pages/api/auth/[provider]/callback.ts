642bdc23229b3352cfc53e0a85348630a590d46b
Luca Zoppetti <luca@luckee.dev>
2021-11-19 20:01:08 +0100

    fix: reintroduced iron-session

--- apps/web/pages/api/auth/[provider]/callback.ts
+++ apps/web/pages/api/auth/[provider]/callback.ts
@@ -1,11 +1,11 @@
 import { StrapiAuthResponse } from '@csl/types';
 import { environment } from '@/environments/environment';
 import { serialize } from 'cookie';
-import { NextApiRequest, NextApiResponse } from 'next';
-
+import { NextApiResponse } from 'next';
+import { withIronSessionApiRoute } from 'iron-session/next';
 const AUTH_URL = `${environment.strapi}/auth`;
 
-function setCookieToken(res: NextApiResponse<any>, token: string) {
+function setCookieToken(res: NextApiResponse<unknown>, token: string) {
 	// @see https://stackoverflow.com/a/57562908
 	res.setHeader(
 		'Set-Cookie',
@@ -20,16 +20,29 @@ function setCookieToken(res: NextApiResponse<any>, token: string) {
 	);
 }
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
-	const { access_token, provider } = req.query;
+export default withIronSessionApiRoute(
+	async (req, res) => {
+		const { access_token, provider } = req.query;
 
-	const url = `${AUTH_URL}/${provider}/callback?access_token=${access_token}`;
+		const url = `${AUTH_URL}/${provider}/callback?access_token=${access_token}`;
 
-	const data: StrapiAuthResponse = await fetch(url).then((res) => res.json());
+		const { jwt, user }: StrapiAuthResponse = await fetch(url).then((res) =>
+			res.json()
+		);
 
-	setCookieToken(res, data.jwt);
-	res.redirect('/');
-}
+		// Run "setCookieToken" first to prevent conflicts with Iron Session
+		setCookieToken(res, jwt);
+
+		// TODO: remove this ugly workaround
+		(req.session as any).user = user;
+
+		await req.session.save();
+
+		if (!user.name) {
+			res.redirect('/auth/signup');
+		} else {
+			res.redirect('/');
+		}
+	},
+	{ cookieName: 'data', password: process.env.COOKIE_SECRET }
+);
