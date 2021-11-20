295caefc4dd8beec4fd508e6d560f22fec4ef462
Luca Zoppetti <luca@luckee.dev>
2021-11-20 17:13:43 +0100

    feat: update logged in user

--- apps/strapi/extensions/users-permissions/controllers/User.js
+++ apps/strapi/extensions/users-permissions/controllers/User.js
@@ -137,4 +137,92 @@ module.exports = {
 
 		ctx.body = sanitizeUser(user);
 	},
+
+	/**
+	 * Update authenticated user.
+	 * @return {Object|Array}
+	 */
+	async updateMe(ctx) {
+		const advancedConfigs = await strapi
+			.store({
+				environment: '',
+				type: 'plugin',
+				name: 'users-permissions',
+				key: 'advanced',
+			})
+			.get();
+
+		const { id } = ctx.state.user;
+		const { email, username, password } = ctx.request.body;
+
+		const user = await strapi.plugins['users-permissions'].services.user.fetch({
+			id,
+		});
+
+		if (_.has(ctx.request.body, 'email') && !email) {
+			return ctx.badRequest('email.notNull');
+		}
+
+		if (_.has(ctx.request.body, 'username') && !username) {
+			return ctx.badRequest('username.notNull');
+		}
+
+		if (
+			_.has(ctx.request.body, 'password') &&
+			!password &&
+			user.provider === 'local'
+		) {
+			return ctx.badRequest('password.notNull');
+		}
+
+		if (_.has(ctx.request.body, 'username')) {
+			const userWithSameUsername = await strapi
+				.query('user', 'users-permissions')
+				.findOne({ username });
+
+			if (userWithSameUsername && userWithSameUsername.id != id) {
+				return ctx.badRequest(
+					null,
+					formatError({
+						id: 'Auth.form.error.username.taken',
+						message: 'username.alreadyTaken.',
+						field: ['username'],
+					})
+				);
+			}
+		}
+
+		if (_.has(ctx.request.body, 'email') && advancedConfigs.unique_email) {
+			const userWithSameEmail = await strapi
+				.query('user', 'users-permissions')
+				.findOne({ email: email.toLowerCase() });
+
+			if (userWithSameEmail && userWithSameEmail.id != id) {
+				return ctx.badRequest(
+					null,
+					formatError({
+						id: 'Auth.form.error.email.taken',
+						message: 'Email already taken',
+						field: ['email'],
+					})
+				);
+			}
+			ctx.request.body.email = ctx.request.body.email.toLowerCase();
+		}
+
+		let updateData = {
+			...ctx.request.body,
+		};
+
+		if (_.has(ctx.request.body, 'password') && password === user.password) {
+			delete updateData.password;
+		}
+
+		const data = await strapi.plugins['users-permissions'].services.user.edit(
+			{ id },
+			updateData
+		);
+
+		ctx.send(sanitizeUser(data));
+	},
 };
