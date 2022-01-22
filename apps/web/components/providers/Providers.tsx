a81a546fa9ea52b24ec425b0dfe6992fae4ccb9d
Luca Zoppetti <luca@luckee.dev>
2022-01-22 16:43:13 +0100

    refactor: providers component

---
+++ apps/web/components/providers/Providers.tsx
@@ -0,0 +1,24 @@
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { ReactNode } from 'react';
+
+interface ProvidersProps {
+	children: ReactNode;
+	session: Session;
+}
+
+export default function Providers(props: ProvidersProps) {
+	return (
+		<SessionProvider session={props.session}>
+			<MantineProvider
+				theme={{
+					colorScheme: 'dark',
+				}}
+			>
+				<NotificationsProvider>{props.children}</NotificationsProvider>
+			</MantineProvider>
+		</SessionProvider>
+	);
+}
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider, NormalizeCSS, GlobalStyles } from '@mantine/core';
-import { NotificationsProvider } from '@mantine/notifications';
-import Wrapper from '../components/wrapper/Wrapper';
-import { SessionProvider } from 'next-auth/react';
+import { NormalizeCSS, GlobalStyles } from '@mantine/core';
+import Wrapper from 'components/wrapper/Wrapper';
+import Providers from 'components/providers/Providers';
 
 export default function App(props: AppProps) {
 	const {
@@ -21,22 +20,13 @@ export default function App(props: AppProps) {
 				/>
 			</Head>
 
-			<SessionProvider session={session}>
-				<MantineProvider
-					theme={{
-						colorScheme: 'dark',
-					}}
-				>
-					<Wrapper>
-						<NormalizeCSS />
-						<GlobalStyles />
-
-						<NotificationsProvider>
-							<Component {...pageProps} />
-						</NotificationsProvider>
-					</Wrapper>
-				</MantineProvider>
-			</SessionProvider>
+			<Providers session={session}>
+				<NormalizeCSS />
+				<GlobalStyles />
+				<Wrapper>
+					<Component {...pageProps} />
+				</Wrapper>
+			</Providers>
 		</>
 	);
 }
