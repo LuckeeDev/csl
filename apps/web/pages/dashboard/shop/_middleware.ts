0c0106c223e5c38f2922715c7ef3f9de09b0c661
Luca Zoppetti <luca@luckee.dev>
2022-03-08 16:51:13 +0100

    refactor: import withAuth as default export

--- apps/web/pages/dashboard/shop/_middleware.ts
+++ apps/web/pages/dashboard/shop/_middleware.ts
@@ -1,5 +1,5 @@
 import { Permission } from '@prisma/client';
-import { withAuth } from 'next-auth/middleware';
+import withAuth from 'next-auth/middleware';
 import checkPermissions from 'utils/auth/checkPermissions';
 
 export default withAuth({
