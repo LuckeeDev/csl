cd47bd455f47475ec6bf59934acd19ed967d3105
Luca Zoppetti <luca@luckee.dev>
2022-03-30 20:08:02 +0200

    feat(events): create time slots

--- apps/web/pages/dashboard/events/slots/new.tsx
+++ apps/web/pages/dashboard/events/slots/new.tsx
@@ -1,8 +1,11 @@
-import { Event } from '@prisma/client';
+import { showNotification, updateNotification } from '@mantine/notifications';
+import { CheckIcon } from '@modulz/radix-icons';
+import { Event, TimeSlot } from '@prisma/client';
 import DashboardPageContainer from 'components/containers/DashboardPageContainer';
 import TimeSlotForm from 'components/forms/TimeSlotForm';
 import PageTitle from 'components/head/PageTitle';
 import getEndpoint from 'data/api/getEndpoint';
+import { createTimeSlot } from 'data/api/timeSlots';
 import useTimeSlotForm, {
 	TimeSlotFormValues,
 } from 'hooks/forms/useTimeSlotForm';
@@ -11,9 +14,16 @@ import useSWR from 'swr';
 
 function DashboardTimeSlotsNew() {
 	const form = useTimeSlotForm();
+	const { data: timeSlots, mutate } = useSWR<TimeSlot[]>('/api/time-slots');
 	const { data: events } = useSWR('/api/events', getEndpoint<Event[]>());
 
-	function onSubmit({ startTime, endTime, ...data }: TimeSlotFormValues) {
+	async function onSubmit({ startTime, endTime, ...data }: TimeSlotFormValues) {
+		showNotification({
+			id: 'create-time-slot',
+			loading: true,
+			message: 'Operazione in corso...',
+		});
+
 		const startHours = startTime.getHours();
 		const startMinutes = startTime.getMinutes();
 
@@ -26,7 +36,34 @@ function DashboardTimeSlotsNew() {
 		data.endDate.setHours(endHours);
 		data.endDate.setMinutes(endMinutes);
 
-		console.log(data);
+		const now = new Date();
+
+		const newTimeSlot: TimeSlot = {
+			created_at: now,
+			updated_at: now,
+			id: 'new',
+			start: data.startDate,
+			end: data.endDate,
+			...data,
+		};
+
+		const optimisticData = timeSlots
+			? [...timeSlots, newTimeSlot]
+			: [newTimeSlot];
+
+		await mutate(createTimeSlot(data), { revalidate: false, optimisticData });
+
+		updateNotification({
+			id: 'create-time-slot',
+			color: 'teal',
+			icon: <CheckIcon />,
+			loading: false,
+			message:
+				'Operazione completata con successo, ora puoi collegare dei seminari',
+			title: 'Fascia oraria creata',
+		});
+
+		form.reset();
 	}
 
 	return (
