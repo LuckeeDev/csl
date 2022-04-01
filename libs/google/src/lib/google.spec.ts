64384500dced37b60ab442bb0e16c93d5b085d87
Luca Zoppetti <luca@luckee.dev>
2022-04-01 17:42:28 +0200

    feat(google): create google lib

---
+++ libs/google/src/lib/google.spec.ts
@@ -0,0 +1,7 @@
+import { google } from './google';
+
+describe('google', () => {
+	it('should work', () => {
+		expect(google()).toEqual('google');
+	});
+});
---
+++ libs/google/src/lib/google.ts
@@ -0,0 +1,3 @@
+export function google(): string {
+	return 'google';
+}
