68dbeac434aa0a291fc105337487dcb84ff2a68b
Luca Zoppetti <luca@luckee.dev>
2022-11-02 10:59:49 +0100

    fix: migrate to root middleware

---
+++ apps/web/middleware.ts
@@ -0,0 +1,37 @@
+import { Permission } from '@prisma/client';
+import { withAuth } from 'next-auth/middleware';
+import checkPermissions from 'utils/auth/checkPermissions';
+
+export default withAuth({
+	callbacks: {
+		authorized: ({ token, req }) => {
+			if (req.nextUrl.pathname.startsWith('/dashboard/shop')) {
+				return checkPermissions(token, [Permission.SHOP_MANAGER]);
+			}
+
+			if (req.nextUrl.pathname.startsWith('/dashboard/groups/all')) {
+				return checkPermissions(token, [Permission.USERS_MANAGER]);
+			}
+
+			if (req.nextUrl.pathname.startsWith('/dashboard/articles')) {
+				return checkPermissions(token, [Permission.NEWS_EDITOR]);
+			}
+
+			if (token) {
+				return true;
+			}
+
+			return false;
+		},
+	},
+});
+
+export const config = {
+	matcher: [
+		'/shop/:path*',
+		'/invite/:path*',
+		'/events/:path*',
+		'/articles/:path*',
+		'/dashboard/:path*',
+	],
+};
