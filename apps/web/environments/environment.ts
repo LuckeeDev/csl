ba34c23b19ba7823d14d7384ae60a862f607f197
Luca Zoppetti <luca@luckee.dev>
2023-01-23 12:43:46 +0100

    fix: allowed email domains bug

--- apps/web/environments/environment.ts
+++ apps/web/environments/environment.ts
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-const ALLOWED_EMAIL_DOMAINS: string = process.env.ALLOWED_EMAIL_DOMAINS!;
-
 export const environment = {
 	url: process.env.NEXT_PUBLIC_BASE_URL!,
 	aws: {
@@ -12,5 +10,5 @@ export const environment = {
 		clientId: process.env.GOOGLE_CLIENT_ID!,
 		secret: process.env.GOOGLE_CLIENT_SECRET!,
 	},
-	allowedEmailDomains: ALLOWED_EMAIL_DOMAINS.split(','),
+	allowedEmailDomains: process.env.ALLOWED_EMAIL_DOMAINS?.split(',') ?? [],
 };
