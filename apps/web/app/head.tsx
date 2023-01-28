d8b85557798128897c2f93b4978c415db2853d1b
Luca Zoppetti <luckeedev@gmail.com>
2023-01-28 16:46:28 +0100

    fix: setup app dir

---
+++ apps/web/app/head.tsx
@@ -0,0 +1,28 @@
+export default function Head() {
+	return (
+		<>
+			<meta
+				name="viewport"
+				content="minimum-scale=1, initial-scale=1, width=device-width"
+			/>
+			<link rel="icon" href="/favicon.ico" />
+
+			<title>Comitato Studentesco Lussana</title>
+			<meta property="og:title" content="Comitato Studentesco Lussana" />
+
+			<meta
+				name="description"
+				content="Sito del Comitato Studentesco del Liceo Lussana di Bergamo: accedi a tutte le attività riguardanti la vita scolastica da un unico posto!"
+			/>
+			<meta
+				property="og:description"
+				content="Sito del Comitato Studentesco del Liceo Lussana di Bergamo: accedi a tutte le attività riguardanti la vita scolastica da un unico posto!"
+			/>
+
+			<meta property="og:site_name" content="cslussana.com" />
+			<meta property="og:type" content="website" />
+			<meta property="og:image" content="/icons/icon-192x192.png" />
+			<meta property="og:url" content="https://cslussana.com" />
+		</>
+	);
+}
