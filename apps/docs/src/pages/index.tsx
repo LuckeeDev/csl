62f4131241db3a35f974a0f4fe30ef71bac71528
Luca Zoppetti <luckeedev@gmail.com>
2023-02-23 12:41:05 +0100

    docs: index link

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -16,10 +16,7 @@ function HomepageHeader() {
 				<h1 className="hero__title">{siteConfig.title}</h1>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
-					<Link
-						className="button button--secondary button--lg"
-						to="/docs/category/setup"
-					>
+					<Link className="button button--secondary button--lg" to="/docs">
 						Setup - 5min ⏱️
 					</Link>
 				</div>
